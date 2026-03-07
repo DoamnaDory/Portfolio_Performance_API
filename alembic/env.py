693bdeae9f891df841bd95c1b62ba70a20855b89
Daria Eremeeva <yeremeeva-d@bk.ru>
2026-03-07 22:48:45 +0300

    Добавила файл с работающей миграцией, теперь таблицы вручную создавать не надо

--- alembic/env.py
+++ alembic/env.py
@@ -25,7 +25,6 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        render_as_batch=True,
     )
 
     with context.begin_transaction():
@@ -33,7 +32,7 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata, render_as_batch=True)
+    context.configure(connection=connection, target_metadata=target_metadata)
 
     with context.begin_transaction():
         context.run_migrations()
